85c2b3163c4370ff04f7057790226659686eb281
Angelicale <gianghgle@gmail.com>
2025-06-11 16:15:06 +0100

    fixed navbar

--- src/components/MenuDropDown.jsx
+++ src/components/MenuDropDown.jsx
@@ -9,12 +9,13 @@ import {
   FolderGit2,
   FileText,
 } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
 
 export default function MenuPage() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
@@ -29,63 +30,68 @@ export default function MenuPage() {
     };
   }, []);
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
-    <div className="z-50 animate-fadeout">
-      <DropDownMenu.Root open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
-        <DropDownMenu.Trigger className="items-center">
-          <div aria-label="navigation menu">
-            <Menu width={30} height={30} />
-          </div>
-        </DropDownMenu.Trigger>
-        <DropDownMenu.Portal>
-          <DropDownMenu.Content
-            sideOffset={25}
-            className="text-black w-screen mr-2 rounded-md bg-[--background] p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade data-[side=right]:animate-slideLeftAndFade data-[side=top]:animate-slideDownAndFade flex flex-col items-center"
+    <DropDownMenu.Root open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
+      <DropDownMenu.Trigger className="items-center">
+        <div aria-label="navigation menu">
+          <Menu width={30} height={30} />
+        </div>
+      </DropDownMenu.Trigger>
+
+      <DropDownMenu.Portal>
+        <DropDownMenu.Content
+          sideOffset={25}
+          className="text-black w-screen mr-2 rounded-md bg-[--background] p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade data-[side=right]:animate-slideLeftAndFade data-[side=top]:animate-slideDownAndFade flex flex-col items-center z-40"
+        >
+          <Link href="/">
+            <DropDownMenu.Item className="relative flex h-[45px] select-none items-center rounded-[3px] px-4 text-[20px] leading-none text-darktext outline-none  hover:text-[--pink]">
+              <div className="grid grid-cols-[auto,auto] gap-2 items-center">
+                <House />
+                <p>Home</p>
+              </div>
+            </DropDownMenu.Item>
+          </Link>
+          <Link href="/about">
+            <DropDownMenu.Item className="relative flex h-[45px] select-none items-center rounded-[3px] px-4 text-[20px] leading-none text-darktext outline-none hover:text-[--pink]">
+              <div className="grid grid-cols-[auto,auto] gap-2 items-center">
+                <CircleUserRound />
+                <p>About Me</p>
+              </div>
+            </DropDownMenu.Item>
+          </Link>
+          <Link href="/project">
+            <DropDownMenu.Item className="relative flex h-[45px] select-none items-center rounded-[3px] px-4 text-[20px] leading-none text-darktext outline-none hover:text-[--pink]">
+              <div className="grid grid-cols-[auto,auto] gap-2 items-center">
+                <FolderGit2 />
+                <p>My Projects</p>
+              </div>
+            </DropDownMenu.Item>
+          </Link>
+          <Link href="/resume">
+            <DropDownMenu.Item className="relative flex h-[45px] select-none items-center rounded-[3px] px-4 text-[20px] leading-none text-darktext outline-none hover:text-[--pink]">
+              <div className="grid grid-cols-[auto,auto] gap-2 items-center">
+                <FileText />
+                <p>My Resume</p>
+              </div>
+            </DropDownMenu.Item>
+          </Link>
+          <Link
+            href="https://github.com/Angelica2310?tab=repositories"
+            target="_blank"
           >
-            <Link href="/">
-              <DropDownMenu.Item className="relative flex h-[45px] select-none items-center rounded-[3px] px-4 text-[20px] leading-none text-darktext outline-none  hover:text-[--pink]">
-                <div className="grid grid-cols-[auto,auto] gap-2 items-center">
-                  <House />
-                  <p>Home</p>
-                </div>
-              </DropDownMenu.Item>
-            </Link>
-            <Link href="/about">
-              <DropDownMenu.Item className="relative flex h-[45px] select-none items-center rounded-[3px] px-4 text-[20px] leading-none text-darktext outline-none hover:text-[--pink]">
-                <div className="grid grid-cols-[auto,auto] gap-2 items-center">
-                  <CircleUserRound />
-                  <p>About Me</p>
-                </div>
-              </DropDownMenu.Item>
-            </Link>
-            <Link href="/project">
-              <DropDownMenu.Item className="relative flex h-[45px] select-none items-center rounded-[3px] px-4 text-[20px] leading-none text-darktext outline-none hover:text-[--pink]">
-                <div className="grid grid-cols-[auto,auto] gap-2 items-center">
-                  <FolderGit2 />
-                  <p>My Projects</p>
-                </div>
-              </DropDownMenu.Item>
-            </Link>
-            <Link href="/resume">
-              <DropDownMenu.Item className="relative flex h-[45px] select-none items-center rounded-[3px] px-4 text-[20px] leading-none text-darktext outline-none hover:text-[--pink]">
-                <div className="grid grid-cols-[auto,auto] gap-2 items-center">
-                  <FileText />
-                  <p>My Resume</p>
-                </div>
-              </DropDownMenu.Item>
-            </Link>
-            <Link
-              href="https://github.com/Angelica2310?tab=repositories"
-              target="_blank"
-            >
-              <DropDownMenu.Item className="relative flex h-[35px] select-none px-4 leading-none text-darktext outline-none gap-2 items-center border border-[--pink] bg-[rgba(195,79,90,0.5)] hover:bg-[rgba(195,79,90,0.8)] cursor-pointer text-white w-fit rounded-md hover:-translate-y-[2px] hover:transition-transform my-3">
-                <FolderOpenDot />
-                <Star />
-              </DropDownMenu.Item>
-            </Link>
-          </DropDownMenu.Content>
-        </DropDownMenu.Portal>
-      </DropDownMenu.Root>
-    </div>
+            <DropDownMenu.Item className="relative flex h-[35px] select-none px-4 leading-none text-darktext outline-none gap-2 items-center border border-[--pink] bg-[rgba(195,79,90,0.5)] hover:bg-[rgba(195,79,90,0.8)] cursor-pointer text-white w-fit rounded-md hover:-translate-y-[2px] hover:transition-transform my-3">
+              <FolderOpenDot />
+              <Star />
+            </DropDownMenu.Item>
+          </Link>
+        </DropDownMenu.Content>
+      </DropDownMenu.Portal>
+    </DropDownMenu.Root>
   );
 }
