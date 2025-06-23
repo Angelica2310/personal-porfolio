70ac353021bea562ec2e6c013d6acadc2b04b006
Angelicale <gianghgle@gmail.com>
2025-06-23 11:24:18 +0100

    removed loading spinner

--- src/app/page.js
+++ src/app/page.js
@@ -6,26 +6,26 @@ import { useEffect, useState } from "react";
 import { DotLoader } from "react-spinners";
 
 export default function HomePage() {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-    return () => clearTimeout(timeout);
-  }, []);
+  // const [isLoading, setIsLoading] = useState(true);
+  // useEffect(() => {
+  //   const timeout = setTimeout(() => {
+  //     setIsLoading(false);
+  //   }, 2000);
+  //   return () => clearTimeout(timeout);
+  // }, []);
   return (
     <>
-      {isLoading ? (
+      {/* {isLoading ? (
         <div className="flex pt-[40%] md:pt-[20%] justify-center">
           <DotLoader color="var(--pink)" size={50} />
         </div>
-      ) : (
-        <div>
-          <Introduction />
-          <Introduce />
-          <ConnectMe />
-        </div>
-      )}
+      ) : ( */}
+      <div>
+        <Introduction />
+        <Introduce />
+        <ConnectMe />
+      </div>
+      {/* )} */}
     </>
   );
 }
